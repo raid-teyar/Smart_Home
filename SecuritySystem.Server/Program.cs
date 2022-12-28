f63dbc9ec245873159a632b9c297c701786d67c8
rd07 <rd07gamer@gmail.com>
2022-12-29 00:18:57 +0100

    removed old security system project

--- SecuritySystem.Server/Program.cs
+++ SecuritySystem.Server/Program.cs
@@ -1,15 +1,26 @@
-namespace SecuritySystem.Server
+﻿using SecuritySystem.Server.Services;
+using SmartHome.Contracts.Contracts;
+
+var builder = WebApplication.CreateBuilder();
+
+// register the contract implementation
+builder.Services.AddTransient<Authorization>();
+
+// Add services to the container.
+builder.Services.AddServiceModelServices();
+
+// bind the app to a port
+builder.WebHost.UseNetTcp(5000);
+
+var app = builder.Build();
+
+// configure CoreWCF endpoints
+app.UseServiceModel(serviceBuilder =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
-            var app = builder.Build();
+    serviceBuilder.AddService<Authorization>();
 
-            app.MapGet("/", () => "Hello World!");
+    serviceBuilder.AddServiceEndpoint<Authorization, ISecurityServiceAuthorization>(
+        new NetTcpBinding(), "net.tcp://localhost:5000/Authorization");
+});
 
-            app.Run();
-        }
-    }
-}
+app.Run();
