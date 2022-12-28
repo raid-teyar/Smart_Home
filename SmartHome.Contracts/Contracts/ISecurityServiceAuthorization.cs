cb7636773d31e8aa3151202257e20f17ff356daa
rd07 <rd07gamer@gmail.com>
2022-12-29 00:15:04 +0100

    feat: added contracts project to wrap servince interfaces and type classes / enums

---
+++ SmartHome.Contracts/Contracts/ISecurityServiceAuthorization.cs
@@ -0,0 +1,18 @@
+﻿using CoreWCF;
+using SecuritySystem.UI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHome.Contracts.Contracts
+{
+    [ServiceContract]
+    public interface ISecurityServiceAuthorization
+    {
+        [OperationContract]
+        User Login(string username, string password);
+    }
+}
---
+++ SmartHome.Contracts/Models/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecuritySystem.UI.Models
+{
+    [DataContract]
+    public class User
+    {
+        [DataMember]
+        public int Id { get; set; }
+
+        [DataMember]
+        public string Email { get; set; }
+
+        [DataMember]
+        public string FullName { get; set; }
+
+        [DataMember]
+        public string Password { get; set; }
+        
+        [DataMember]
+        public UserType UserType { get; set; }
+
+
+        public override string ToString()
+        {
+            return $"{Id} {Email} {FullName} {Password} {UserType}";
+        }
+
+    }
+}
