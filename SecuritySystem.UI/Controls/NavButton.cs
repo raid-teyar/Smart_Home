7f17b398055282e9c7737964709f0e5243439297
rd07 <rd07gamer@gmail.com>
2022-12-29 00:13:51 +0100

    feat : added custom controls (navbar, switch button)

---
+++ SecuritySystem.UI/Controls/NavButton.cs
@@ -0,0 +1,45 @@
+﻿using MahApps.Metro.IconPacks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SecuritySystem.UI.Controls
+{
+
+    public class NavButton : ListBoxItem
+    {
+        static NavButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(NavButton), new FrameworkPropertyMetadata(typeof(NavButton)));
+        }
+
+        public string Header
+        {
+            get { return (string)GetValue(HeaderProperty); }
+            set { SetValue(HeaderProperty, value); }
+        }
+
+        public static readonly DependencyProperty HeaderProperty = DependencyProperty.Register("Header", typeof(string), typeof(NavButton), new PropertyMetadata(null));
+
+
+        public string NavLink
+        {
+            get { return (string)GetValue(NavLinkProperty); }
+            set { SetValue(NavLinkProperty, value); }
+        }
+
+        public static readonly DependencyProperty NavLinkProperty = DependencyProperty.Register("NavLink", typeof(string), typeof(NavButton));
+
+    }
+}
