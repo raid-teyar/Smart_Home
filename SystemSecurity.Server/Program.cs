54772fe751c4b2084657b321c436eb0905f15774
rd07 <rd07gamer@gmail.com>
2022-12-29 00:12:05 +0100

    feat! : set up the WCF server using asp.net as the hosting app and tcp for binding

---
+++ SystemSecurity.Server/Program.cs
@@ -0,0 +1,26 @@
+﻿using SecuritySystem.Server.Services;
+using SmartHome.Contracts.Contracts;
+
+var builder = WebApplication.CreateBuilder();
+
+// register the contract implementation
+builder.Services.AddTransient<Authorization>();
+
+// Add services to the container.
+builder.Services.AddServiceModelServices();
+
+// bind the app to a port
+builder.WebHost.UseNetTcp(5000);
+
+var app = builder.Build();
+
+// configure CoreWCF endpoints
+app.UseServiceModel(serviceBuilder =>
+{
+    serviceBuilder.AddService<Authorization>();
+
+    serviceBuilder.AddServiceEndpoint<Authorization, ISecurityServiceAuthorization>(
+        new NetTcpBinding(), "net.tcp://localhost:5000/Authorization");
+});
+
+app.Run();
---
+++ SystemSecurity.Server/Services/Authorization.cs
@@ -0,0 +1,29 @@
+﻿using SmartHome.Contracts.Contracts;
+using System.Data;
+using System.Data.SqlClient;
+using Dapper;
+using SecuritySystem.UI.Models;
+using MySql.Data.MySqlClient;
+using System.ServiceModel;
+
+namespace SecuritySystem.Server.Services
+{
+    public class Authorization : ISecurityServiceAuthorization
+    {
+        public string ConnectionString { get; set; }
+
+        public Authorization()
+        {
+            ConnectionString = "server=localhost;user=root;database=smart home;port=3306;password=raid";
+        }
+
+        public User Login(string email, string password)
+        {
+            using (IDbConnection cnn = new MySqlConnection(ConnectionString))
+            {
+                User user = cnn.Query<User>($"SELECT * FROM user WHERE email = '{email}' AND password = '{password}' ").First();
+                return user;
+            }
+        }
+    }
+}
